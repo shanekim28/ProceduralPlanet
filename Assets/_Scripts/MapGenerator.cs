5b047e8b6cd83a4b1e2c0865e73e826f502c7faa
Shane Kim <shanekim28@gmail.com>
2021-05-13 14:05:46 -0700

    Basic simplex noise generation and texturing

---
+++ Assets/_Scripts/MapGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public MapDisplay display;
+
+    public void GenerateMap() {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        
+        if (display)
+            display.DrawNoiseMap(noiseMap);
+    }
+}
---
+++ Assets/_Scripts/Noise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SimplexNoise;
+using UnityEngine;
+
+public static class Noise {
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+        SimplexNoise.Noise.Seed = 123456789;
+        
+        if (scale <= 0) {
+            scale = 0.0001f;
+        }
+
+        return SimplexNoise.Noise.Calc2D(mapWidth, mapHeight, scale);
+    }
+}
